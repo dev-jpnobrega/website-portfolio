9d70e6ea2496eadd164f36458420722ee5ff2964
João Paulo Nobrega <joao.paulo.nobrega@avanade.com>
2019-07-07 18:36:06 -0300

    cw

--- jenkins/scripts/test.sh
+++ jenkins/scripts/test.sh
@@ -2,17 +2,10 @@
 
 echo 'Inicialize JEST test'
 
-set -x
-npm install -g react-scripts
-set +x
 
 echo 'runner Jest (https://facebook.github.io/jest/).'
 
 cd ./
-
 ls
 
-react-scripts --version
-
-set -x
 npm test
