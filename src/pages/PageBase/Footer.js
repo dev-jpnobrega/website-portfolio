9cd726ad9aefa3c6f127db8c6c8c1b73e7cf2b3f
João Paulo Nobrega <joao.paulo.nobrega@avanade.com>
2019-07-12 14:10:34 -0300

    f

--- src/pages/PageBase/Footer.js
+++ src/pages/PageBase/Footer.js
@@ -5,7 +5,7 @@ function Footer() {
     <footer id="footer">
       <p>
         &copy; All rights reserved. @dev-jpnobrega.<br/>
-        This website is <strong><a href="https://github.com/dev-jpnobrega/website-portfolio" >open source</a></strong> ❤️.
+        This website iss <strong><a href="https://github.com/dev-jpnobrega/website-portfolio" >open source</a></strong> ❤️.
       </p>
     </footer>
   )
